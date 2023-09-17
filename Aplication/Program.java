9586d13df532bf7f2d31e008de3b5228303f574d
Gabriel Dias <gabriel.diasramos@hotmail.com>
2023-09-17 03:33:56 -0300

    Instaciando enums(Duas formas), como transformar o enum em uma String, como ver o typeof da variavel os2

--- Aplication/Program.java
+++ Aplication/Program.java
@@ -8,9 +8,22 @@ import enums.OrderStatus;
 public class Program {
     public static void main(String[] args) {
         
-        Pedido pedido = new Pedido(1080, LocalDate.now(), OrderStatus.PENDING_PAYMENT);
+        Pedido pedido = new Pedido(1080, LocalDate.now(), OrderStatus.PENDING_PAYMENT); 
 
         System.out.println(pedido);
+        OrderStatus os1 = OrderStatus.DELIVERED; //Instanciando um enum
+
+        OrderStatus os2 = OrderStatus.valueOf("SHIPPED"); //Transforma uma String em enum
+
+        System.out.println(os1);
+        System.out.println(os2);
+
+        String teste = OrderStatus.PROCESSING.toString(); //Transforma o enum em String
+
+        System.out.println(teste); 
+
+        //ver o typeof de os2
+        System.out.println(os2.getClass().getSimpleName()); //getClass().getSimpleName() retorna o tipo da variavel
     }
     
 }
