55916a397984b05af2c8424d166602d4fac769d7
Gabriel Dias <gabriel.diasramos@hotmail.com>
2023-09-17 03:22:21 -0300

    exemplos de enums

---
+++ enums/OrderStatus.java
@@ -0,0 +1,19 @@
+package enums;
+
+public enum OrderStatus {
+    // O enum é um tipo especial que serve para especificar de forma literal um conjunto de constantes relacionadas. 
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+
+    //Exemplos de ENUM abaixo.
+    // Dias da semana
+    // Meses do ano
+    // Status de um pedido
+    // Tipo de produto
+    // Tipo de cliente
+    // Tipo de pagamento
+    // Tipo de usuário
+
+}
